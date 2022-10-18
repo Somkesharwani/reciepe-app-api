b1f34785a223774cf5125f393cc32f1d52a617df
Somkesharwani <kesharwanisom14@gmail.com>
2022-10-18 16:20:13 +0100

    Model changes added

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,34 @@
-from django.db import models # noqa
+"""
+Database models
+"""
+import email
+from email.policy import default
+from enum import unique
+from django.db import models
+from django.contrib.auth.models import(
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    """Manager  user field"""
+
+    def create_user(self,email,password=None,**extra_field):
+        """Create , save and return a new user"""
+        user=self.model(email=email,**extra_field)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system"""
+    email=models.EmailField(max_length=255,unique=True)
+    name=models.CharField(max_length=255)
+    is_active=models.BooleanField(default = True)
+    is_staff=models.BooleanField(default=False)
+
+    objects=UserManager()
+
+    USERNAME_FIELD = 'email'
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,22 @@
+"""
+Test for models
+"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    """Test models,"""
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a user with an email is successfully"""
+        email='abc@test.com'
+        password='testpass123'
+        user =get_user_model.objects.create_user(
+            email = email,
+            password = password
+        )
+
+        self.assertEqual(user.email,email)
+        self.assertEqual(user.check_password(password))
