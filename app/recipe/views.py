29443ca79db431bb29415fa5069e7b79dd9ca850
Somkesharwani <kesharwanisom14@gmail.com>
2022-10-26 00:37:01 +0100

    Upload Image

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,14 +1,24 @@
-"""Create View for Recipe API"""
+"""
+Create View for Recipe API
+"""
 
 from rest_framework import (
     viewsets,
-    mixins
+    mixins,
+    status,
 )
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from django.shortcuts import render
 
-from core.models import Recipe,Tag,Ingredient
+from core.models import (
+    Recipe,
+    Tag,
+    Ingredient,
+)
+
 from recipe import serializers
 
 
@@ -27,6 +37,8 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Return the serializer class for request"""
         if self.action == 'list':
             return serializers.RecipeSerializer
+        elif self.action == 'upload_image':
+            return serializers.RecipeImageSerializer
 
         return self.serializer_class
 
@@ -34,31 +46,42 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Create new recipe"""
         serializer.save(user=self.request.user)
 
-class TagViewSet(mixins.DestroyModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.ListModelMixin,
-    viewsets.GenericViewSet):
-    """Manage tag in the database """
-    serializer_class = serializers.TagSerialize
-    queryset = Tag.objects.all()
+    @action(methods=['POST'], detail=True, url_path='upload_image')
+    def upload_image(self, request, pk=None):
+        """Upload an image to recipe"""
+        recipe = self.get_object()
+        serializer = self.get_serializer(recipe,data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status.HTTP_200_OK)
+
+        return Response(serializer.error,status.HTTP_400_BAD_REQUEST)
+
+
+class BaseRecipeAttrViewSet(mixins.DestroyModelMixin,
+                            mixins.UpdateModelMixin,
+                            mixins.ListModelMixin,
+                            viewsets.GenericViewSet):
+
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
+
         return self.queryset.filter(user=self.request.user).order_by('-name')
 
-class IngredientViewSet(mixins.UpdateModelMixin,
-                        mixins.DestroyModelMixin,
-                        mixins.ListModelMixin,
-                        viewsets.GenericViewSet):
+
+class TagViewSet(BaseRecipeAttrViewSet):
+    """Manage tag in the database """
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+
+
+class IngredientViewSet(BaseRecipeAttrViewSet):
     """Manage ingredient i databse"""
     serializer_class = serializers.IngredientSerializer
     queryset = Ingredient.objects.all()
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        return self.queryset.filter(user=self.request.user).order_by('-name')
 
 
 
